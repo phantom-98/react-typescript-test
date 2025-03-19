c9b53dfd411231c6e1df86bc0a482d3309cc584f
Phantom <165128795+phantom-98@users.noreply.github.com>
2025-03-19 19:01:53 +0900

    fix minor problems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,19 @@
 'use client'
-import { useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar, PieChart, Pie, ResponsiveContainer, Cell, Label, Sector } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, BarChart, Bar, PieChart, Pie, Cell, Sector } from 'recharts';
 import { PieSectorDataItem } from 'recharts/types/polar/Pie';
 
-const months = [
-  {
-    name: 'Jan',
-    uv: 26.3,
-    pv: 27.8,
-  },
-  {
-    name: 'Feb',
-    uv: 26.7,
-    pv: 26.4,
-  },
-  {
-    name: 'Mar',
-    uv: 27.8,
-    pv: 26.1,
-  },
-  {
-    name: 'Apr',
-    uv: 27.1,
-    pv: 27.9,
-  },
-  {
-    name: 'May',
-    uv: 26.3,
-    pv: 27.2,
-  },
-  {
-    name: 'Jun',
-    uv: 27.5,
-    pv: 26.2,
-  },
-]
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+
+const months = new Array<number>(21).fill(0).map((_, index) => {
+  const name = month[(index - 1) / 4];
+  const uv = Math.random() * 2 + 26;
+  const pv = Math.random() * 2 + 26;
+  return {
+    name,
+    uv,
+    pv
+  }
+})
 
 const years = [
   {
@@ -74,22 +53,23 @@ const pie = [
   },
 ].reverse();
 
+
 const Home = () => {
-  const [active, setActive] = useState(0);
+  
   return (
     <div className="flex items-center justify-center">
       <div className='mt-16 flex gap-8 relative'>
-        <div className='border-[#343434] border-2 rounded-2xl p-8 w-fit h-fit'>
-          <LineChart width={400} height={300} data={months}>
-            <CartesianGrid vertical={false}></CartesianGrid>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" strokeWidth={2} dot={false}/>
-            <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} dot={false}/>
+        <div className='border-[#343434] border-2 rounded-2xl p-4 pr-8 w-fit h-fit'>
+          <LineChart width={500} height={400} data={months}>
+            <CartesianGrid vertical={false} stroke='#5D5D5D'></CartesianGrid>
+            <Line type="monotone" dataKey="uv" stroke="#6F43E7" strokeWidth={3} dot={false}/>
+            <Line type="monotone" dataKey="pv" stroke="#6F43E7" strokeWidth={3} dot={false}/>
             <XAxis dataKey="name" padding={{left: 10, right: 10}} axisLine={false} tickLine={false} tick={{fill: "white"}} tickMargin={16}/>
             <YAxis type='number' domain={[25, 29]} axisLine={false} tickLine={false} tick={{fill: "white"}} tickMargin={16}/>
           </LineChart>
         </div>
-        <div className='border-[#343434] border-2 rounded-2xl p-8 w-fit h-fit'>
-          <BarChart data={years} width={200} height={250}>
+        <div className='border-[#343434] border-2 rounded-2xl p-4 w-fit h-fit'>
+          <BarChart data={years} width={300} height={330}>
             <Bar dataKey="v" color='color' >
               {
                 years.map((y, index) => (
@@ -100,15 +80,15 @@ const Home = () => {
             <XAxis dataKey="name" axisLine={false} tickLine={false} tick={{fill: "white"}} tickMargin={16}/>
           </BarChart>
         </div>
-        <div className='absolute top-64 left-64 border-[#343434] border-2 rounded-2xl p-8 backdrop-blur-sm'>
+        <div className='absolute top-[360px] left-[360px] border-[#343434] border-2 rounded-2xl p-8 backdrop-blur-sm'>
           <PieChart width={300} height={300}>
             <Pie
               dataKey="v"
+              isAnimationActive={false}
               data={pie}
               cx="50%"
               cy="50%"
-              outerRadius={120}
-              fill="#8884d8"
+              outerRadius={140}
               stroke='none'
               startAngle={85}
               endAngle={450}
@@ -118,7 +98,7 @@ const Home = () => {
             >
               {
                 pie.map((p, index) => (
-                  <Cell key={index} fill={p.color} width={index} radius={80 - 4*index} className='border-none'></Cell>
+                  <Cell key={index} fill={p.color} width={index} className='border-none'></Cell>
                 ))
               }
             </Pie>
